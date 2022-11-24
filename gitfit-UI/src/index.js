8143be38c83dab0c95c48c5fb14ee19985bda925
AndreeaCvl <44116789+AndreeaCvl@users.noreply.github.com>
2022-11-24 12:18:25 +0200

    upd path for clients page in index.js

--- gitfit-UI/src/index.js
+++ gitfit-UI/src/index.js
@@ -7,6 +7,8 @@ import Navbar from './components/Navbar/navbar.js';
 //import components
 import AboutUsPage from "./views/AboutUsPage/AboutUsPage.js";
 import SignUpPage from './views/SignUpPage/SignUpPage.js';
+import ClientsPage from './views/ClientsPage/ClientsPage.js';
+
 
 var hist = createBrowserHistory();
 const root = ReactDOM.createRoot(document.getElementById('root')); 
@@ -16,6 +18,7 @@ root.render(
     <Switch>
       <Route path="/about" component={AboutUsPage} />
       <Route path="/sign-up" component={SignUpPage}/>
+      <Route path='/clients' component={ClientsPage}/>
       <Route path="/" component={AboutUsPage} />
     </Switch>
   </Router>,
